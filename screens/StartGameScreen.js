4f4d04bf3ca4a3f6b9ac0e712b0015259e9232c2
lukakhuza <lukakhuza7@gmail.com>
2024-12-07 03:36:24 -0500

    styling custom buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,34 @@
 import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { StyleSheet } from "react-native";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-      {/* BUTTONS */}
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
